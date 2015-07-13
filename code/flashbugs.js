7047ab245bc4e80ad2d6e72e2a06f9d280324c14
dbirman <danbirman@gmail.com>
2015-07-13 15:36:44 -0700

    Fixed some weird things...

--- code/flashbugs.js
+++ code/flashbugs.js
@@ -73,9 +73,11 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
  
 var colors = ['red','green','blue','yellow','teal','purple','black'];
-var bugCounters = [100,200,500,1000];
+var bugCounters = [100,250,500,1000];
 var posX = [-300,-100,100,300];
-var texts = ['100ms','200ms','500ms','1000ms'];
+var texts = ['100ms','250ms','500ms','1000ms'];
+var actTime = 4000;
+var acting = 0;
 var bugs = [0,0,0,0];
 var onColor = 'red';
 var offColor = 'teal';
@@ -86,16 +88,26 @@ function drawBugs() {
 	time = now();
 	elapsed = time-prev;
 	prev = time;
+	actTime -= elapsed;
+	if (actTime < 0) {
+		actTime = 5000;
+		acting = acting+1;
+		if (acting>3) {
+			acting = 0;
+		}
+	}
+	i = acting;
+	bugs[i]-=elapsed;
+	if (bugs[i] <= -bugCounters[i]) {
+		bugs[i] += bugCounters[i]*2;
+	}
+	if (bugs[i]<0) {
+		drawCircle(posX[i],0,onColor);
+	} else {
+		drawCircle(posX[i],0,offColor);
+	}
 	for (i=0;i<4;i++) {
-		bugs[i]-=elapsed;
-		if (bugs[i] <= -bugCounters[i]) {
-			bugs[i] += bugCounters[i]*2;
-		}
-		if (bugs[i]<0) {
-			drawCircle(posX[i],0,onColor);
-		} else {
-			drawCircle(posX[i],0,offColor);
-		}
+		ctx.font = "20px Arial";
 		ctx.fillStyle = parseColor('black');
 		ctx.fillText(texts[i],cen2canx(posX[i]),cen2cany(100));
 	}
